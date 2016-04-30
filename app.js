caa5905cf45d3942b4d480cd0aac30baae576c0e
troy-lamerton <troy.lamerton@gmail.com>
2016-04-30 12:13:50 +1200

    Escape single and double quotes inside quote content

--- app.js
+++ app.js
@@ -7,7 +7,8 @@
 
 // grab a random quote usign the api
 // log it to the console
-
+var singleQuote = "&#39;";
+var doubleQuote = "&quot;";
 
 $(document).ready(function() {
 
@@ -15,12 +16,19 @@ function newQuote() {
   $.getJSON("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&_jsonp=?", function(quote) {
     $('#quote').html(quote[0].content);
     $('#author').html(" — " + quote[0].title);
-
+    console.log(quote[0].content);
     //replace %data% in tweet link to quote text
     var tweetLink = $('.twitter-share-button').attr("href");
     //remove <p> and </p> tags from quote content
     var escapedQuote = quote[0].content.slice(3, quote[0].content.length - 5);
-    tweetLink = tweetLink.replace("%data%", (escapedQuote + " — " + quote[0].title));
+    //remove <strong> </strong> etc tags
+    
+    console.log(escapedQuote);
+    escapedQuote = escapedQuote.replace(/'/g, singleQuote);
+    escapedQuote = escapedQuote.replace(/"/g, doubleQuote);
+    console.log(escapedQuote);
+    //tweetLink = tweetLink.replace("%data%", (escapedQuote + " — " + quote[0].title));
+    tweetLink = tweetLink.slice(0, 38) + (escapedQuote + " — " + quote[0].title);
     $('.twitter-share-button').attr("href", tweetLink);
   });
 }
@@ -32,5 +40,3 @@ $('.btn').click(function() {
 });
 
 });
-
-
